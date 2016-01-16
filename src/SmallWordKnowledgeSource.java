e44b7bd056d13b4de27d7fce07897f9b9ceb1a92
Alex DeBoni <adeboni@scu.edu>
2016-01-16 12:31:14 -0800

    first commit

---
+++ src/SmallWordKnowledgeSource.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SmallWordKnowledgeSource {
+
+	public static List<String> getWords(int numLetters, int numWords) throws FileNotFoundException {
+		List<String> ret = new ArrayList<String>();
+		
+		Scanner s = new Scanner(new File("resources/words.txt"));
+		ArrayList<String> dict = new ArrayList<String>();
+		while (s.hasNext()) dict.add(s.next());
+		s.close();
+		
+		for (int i = 0; i < dict.size(); i++) {
+			if (dict.get(i).length() == numLetters)
+				ret.add(dict.get(i));
+			if (ret.size() == numWords) break;
+		}
+			
+		return ret;
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		List<String> words = getWords(3, 20);
+		for (String s : words) {
+			System.out.println(s);
+		}
+	}
+
+}
