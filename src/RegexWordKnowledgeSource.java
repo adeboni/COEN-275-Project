baf9823c55b8aa1153d79120f1fad35f5b9ff6b4
Alex DeBoni <adeboni@scu.edu>
2016-01-21 16:15:19 -0800

    added regex knowledge source

---
+++ src/RegexWordKnowledgeSource.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class RegexWordKnowledgeSource {
+
+	public static List<String> getWords(String regex, int numWords) throws FileNotFoundException {
+		List<String> ret = new ArrayList<String>();
+		
+		Scanner s = new Scanner(new File("resources/words.txt"));
+		ArrayList<String> dict = new ArrayList<String>();
+		while (s.hasNext()) dict.add(s.next());
+		s.close();
+		
+		for (int i = 0; i < dict.size(); i++) {
+			if (Pattern.matches(regex, dict.get(i)))
+				ret.add(dict.get(i));
+			if (ret.size() == numWords) break;
+		}
+			
+		return ret;
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		List<String> words = getWords("[ahf].[b].*", 10);
+		for (String s : words) {
+			System.out.println(s);
+		}
+	}
+
+}
