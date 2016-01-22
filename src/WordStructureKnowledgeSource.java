8b1ae9e434c79bc079a439166e73fc7d2632413c
Alex DeBoni <adeboni@scu.edu>
2016-01-21 16:23:25 -0800

    added word structure knowledge source

---
+++ src/WordStructureKnowledgeSource.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class WordStructureKnowledgeSource {
+
+	public static enum StructureLocation {
+		PREFIX,
+		SUFFIX
+	}
+	
+	public static List<String> getWords(StructureLocation location, int numLetters, int numWords) throws FileNotFoundException {
+		List<String> ret = new ArrayList<String>();
+		
+		ArrayList<String> dict = new ArrayList<String>();
+		if (location == StructureLocation.PREFIX) {
+			Scanner s = new Scanner(new File("resources/prefixes.txt"));
+			while (s.hasNext()) dict.add(s.next());
+			s.close();
+		} else {
+			Scanner s = new Scanner(new File("resources/suffixes.txt"));
+			while (s.hasNext()) dict.add(s.next());
+			s.close();
+		}
+		
+		for (int i = 0; i < dict.size(); i++) {
+			if (dict.get(i).length() == numLetters)
+				ret.add(dict.get(i));
+			if (ret.size() == numWords) break;
+		}
+			
+		return ret;
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		List<String> words = getWords(StructureLocation.SUFFIX, 3, 20);
+		for (String s : words) {
+			System.out.println(s);
+		}
+	}
+
+}
