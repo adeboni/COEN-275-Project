14f1390606eaee2b5a60c5e2b3bfca77811d51bf
Alex Wong <sfalexw@gmail.com>
2016-01-25 13:37:56 -0800

    uploading assumption and assertion

--- src/domain/Assertion.java
+++ src/domain/Assertion.java
@@ -1,5 +1,18 @@
+/* src/domain/Assertion
+ * ------------------------------
+ * Written by: Sui Fung Alex Wong
+ *       Date: Jan 25, 2016
+ */
+
 package domain;
 
-public class Assertion {
-   // TODO
+public class Assertion extends Assumption {
+   /**
+    * @return false
+    *             as Assertion is not retractable
+    */
+   @Override
+   public boolean isRetractable() {
+      return false;
+   }
 }
--- src/domain/Assumption.java
+++ src/domain/Assumption.java
@@ -1,5 +1,95 @@
+/* src/domain/Assumption
+ * ------------------------------
+ * Written by: Sui Fung Alex Wong
+ *       Date: Jan 25, 2016
+ */
+
 package domain;
 
-public class Assumption {
-   // TODO
+import knowledge.KnowledgeSource;
+
+public class Assumption extends BlackboardObject {
+   private BlackboardObject target;
+   private KnowledgeSource creator;
+   private String reason;
+   private char plainLetter;
+   private char cipherLetter;
+   
+   /**
+    * @return true
+    *             as Assumption is always retractable
+    */
+   public boolean isRetractable() {
+      return true;
+   }
+
+   /**
+    * @return the target
+    */
+   public BlackboardObject getTarget() {
+      return target;
+   }
+
+   /**
+    * @param target the target to set
+    */
+   public void setTarget(BlackboardObject target) {
+      this.target = target;
+   }
+
+   /**
+    * @return the creator
+    */
+   public KnowledgeSource getCreator() {
+      return creator;
+   }
+
+   /**
+    * @param creator the creator to set
+    */
+   public void setCreator(KnowledgeSource creator) {
+      this.creator = creator;
+   }
+
+   /**
+    * @return the reason
+    */
+   public String getReason() {
+      return reason;
+   }
+
+   /**
+    * @param reason the reason to set
+    */
+   public void setReason(String reason) {
+      this.reason = reason;
+   }
+
+   /**
+    * @return the plainLetter
+    */
+   public char getPlainLetter() {
+      return plainLetter;
+   }
+
+   /**
+    * @param plainLetter the plainLetter to set
+    */
+   public void setPlainLetter(char plainLetter) {
+      this.plainLetter = plainLetter;
+   }
+
+   /**
+    * @return the cipherLetter
+    */
+   public char getCipherLetter() {
+      return cipherLetter;
+   }
+
+   /**
+    * @param cipherLetter the cipherLetter to set
+    */
+   public void setCipherLetter(char cipherLetter) {
+      this.cipherLetter = cipherLetter;
+   }
 }
