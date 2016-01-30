02e63898673f6465c5194ddf511886cb767ae197
Alex Wong <sfalexw@gmail.com>
2016-01-29 21:26:35 -0800

    Update DoubleLetterKnowledgeSource.evaluate()

--- src/knowledge/sources/DoubleLetterKnowledgeSource.java
+++ src/knowledge/sources/DoubleLetterKnowledgeSource.java
@@ -9,7 +9,9 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 import blackboard.Blackboard;
 import blackboard.BlackboardContext;
+import domain.Assertion;
 import domain.Assumption;
+import domain.CipherLetter;
 import domain.Sentence;
 import domain.Word;
 import util.SentenceUtil;
@@ -23,36 +25,67 @@ public class DoubleLetterKnowledgeSource extends StringKnowledgeSource {
 
     @Override
     public void evaluate() {
-    	Blackboard blackboard = BlackboardContext.getInstance().getBlackboard();
+        Blackboard blackboard = BlackboardContext.getInstance().getBlackboard();
         Sentence sentence = blackboard.getSentence();
         ConcurrentLinkedQueue<Assumption> queue = this.getPastAssumptions();
         List<Word> words = SentenceUtil.getWords(sentence);
+        List<CipherLetter> letters;
+        String wordStr;
         
+        boolean found;
+        int index;
+        Assertion a;
         
+        // demo only
+        for (Word word : words) {
+           letters = SentenceUtil.getLetters(word);
+           wordStr = word.value();
+           index = 0;
+           found = false;
+           for (int i = 0; i < wordStr.length() - 2; ++i) {
+              if (wordStr.charAt(i) == wordStr.charAt(i + 1)) {
+                 found = true;
+                 index = i;
+                 break;
+              }
+           }
+           
+           if (found) {
+              if (index != 0 && index != wordStr.length() - 3) { // middle
+                 a = new Assertion();
+                 a.setCipherLetter("S");
+                 a.setPlainLetter("E");
+                 queue.add(a);
+                 history.add("S");
+              }
+           }
+        }
     }
 
     public List<String> doubleLetter(String word, int numWords) throws FileNotFoundException {
-        //if Two back to back same letters in the middle of the a word are likely 2 vowels > "EE" or "OO" especially in a 4 char word
+        // if Two back to back same letters in the middle of the a word are likely 2 vowels > "EE" or "OO" especially in a 4 char word
         ArrayList<String> dict = new ArrayList<>();
         List<String> ret = new ArrayList<String>();
-        int i = 0;
-        boolean found=false;
-        for (i = 0; i < word.length()-1; i++) {
-            if (word.charAt(i) == word.charAt(i+1)) {
-                found=true;
+        boolean found = false;
+        
+        for (int i = 0; i < word.length() - 1; ++i) {
+            if (word.charAt(i) == word.charAt(i + 1)) {
+                found = true;
                 break;
             }
         }
-        if(found) {
+        
+        if (found) {
             Scanner s = new Scanner(new File("resources/doubleLetters.txt"));
             while (s.hasNext()) dict.add(s.next());
             s.close();
         }
-        for (int k = 0; k < numWords; k++) {
+        
+        for (int k = 0; k < numWords; ++k) {
             ret.add(dict.get(k));
-
-    }
-    return ret;
+        }
+        
+        return ret;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
